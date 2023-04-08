3ae692c4c7cd1de1a871b61f0b496499776f2a81
byungseon <byungseon.kang@gmail.com>
2023-04-08 23:26:53 +0900

    perf: modify calendar page scroll

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -3,33 +3,28 @@ import Week from "@/components/Calendar/Week";
 
 import InfiniteScrollContainer from "@/components/InfiniteScrollContainer";
 import useWeeks from "@/hooks/useWeeks";
-import { useLayoutEffect, useMemo, useRef } from "react";
+import { useLayoutEffect, useMemo } from "react";
 
 const CalendarPage: NextPage = () => {
-  const wrapper = useRef<HTMLDivElement>(null);
   const { weeks, isFetchingPrev, isFetchingNext, getPrevWeeks, getNextWeeks } =
     useWeeks();
 
   const firstWeekNumber = useMemo(() => weeks[0]?.number || 0, [weeks]);
 
   useLayoutEffect(() => {
-    const wrapperCurrent = wrapper.current;
-    if (!wrapperCurrent) return;
-    wrapperCurrent.scrollTo({ top: 640, behavior: "auto" });
+    window.scrollTo({ top: 640, behavior: "auto" });
   }, [firstWeekNumber]);
 
   return (
-    <div ref={wrapper} className="h-[100vh] overflow-auto">
-      <InfiniteScrollContainer
-        disabled={isFetchingPrev || isFetchingNext}
-        onReachedStart={getPrevWeeks}
-        onReachedEnd={getNextWeeks}
-      >
-        {weeks.map((week) => (
-          <Week key={`${week.year}-${week.number}`} {...week} />
-        ))}
-      </InfiniteScrollContainer>
-    </div>
+    <InfiniteScrollContainer
+      disabled={isFetchingPrev || isFetchingNext}
+      onReachedStart={getPrevWeeks}
+      onReachedEnd={getNextWeeks}
+    >
+      {weeks.map((week) => (
+        <Week key={`${week.year}-${week.number}`} {...week} />
+      ))}
+    </InfiniteScrollContainer>
   );
 };
 
