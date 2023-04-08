21189a9187b69bb8642ac85b9dec450b1b71c5f3
kangbyungseon <byungseon.kang@stradvision.com>
2023-04-08 15:01:33 +0900

    feat: modify initial page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,15 @@
-const IndexPage = () => {
-  const idea = [
-    {
-      title: "Calendar",
-      contents: [
-        "Log: book.readStartAt",
-        "Log: book.readEndAt",
-        "Log: comment.createdAt",
-      ],
-    },
-    {
-      title: "Book",
-      contents: ["Func: Search Book", "Func: Add ReadingBook", "Func: "],
-    },
-    {
-      title: "BookComment",
-      contents: ["Func: Add BookComment (book, page, line, content)"],
-    },
-  ];
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-  return (
-    <div>
-      {idea.map(({ title, contents }) => (
-        <section key={title}>
-          <ul>
-            <h2>{title}</h2>
-            {contents.map((content) => (
-              <li key={content}>{content}</li>
-            ))}
-          </ul>
-        </section>
-      ))}
-    </div>
-  );
+const IndexPage: NextPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/books/search");
+  }, [router]);
+
+  return null;
 };
 
 export default IndexPage;
