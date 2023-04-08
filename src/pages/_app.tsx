20a773cdeceb85c3dc02872ec1f6b7fc07f68535
byungseon <byungseon.kang@gmail.com>
2023-04-08 23:26:41 +0900

    feat: modify _app layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 
+import GlobalNavigation from "@/components/Navigation/GlobalNavigation";
+
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient({
     defaultOptions: {
@@ -11,7 +13,12 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <div className="flex flex-col">
+        <div className="flex-1 pb-16">
+          <Component {...pageProps} />
+        </div>
+        <GlobalNavigation />
+      </div>
     </QueryClientProvider>
   );
 }
