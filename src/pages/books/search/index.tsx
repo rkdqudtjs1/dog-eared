4a0076f1d8237925fd3ffb29634ba4754c8a0aa0
kangbyungseon <byungseon.kang@stradvision.com>
2023-04-08 16:50:47 +0900

    feat: update favicon, document title

--- src/pages/books/search/index.tsx
+++ src/pages/books/search/index.tsx
@@ -3,6 +3,7 @@ import InfiniteScrollContainer from "@/components/InfiniteScrollContainer";
 import useDebouncedValue from "@/hooks/useDebouncedValue";
 import useSearchBooks from "@/hooks/useSearchBooks";
 import { NextPage } from "next";
+import Head from "next/head";
 import { useState } from "react";
 
 const BookSearchPage: NextPage = () => {
@@ -15,26 +16,31 @@ const BookSearchPage: NextPage = () => {
   );
 
   return (
-    <div className="flex flex-col">
-      <div className="sticky top-0 z-10">
-        <input
-          className="w-full p-2"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </div>
-
-      <InfiniteScrollContainer
-        onReachedEnd={fetchNextPage}
-        disabled={isFetching || !hasNextPage}
-      >
-        <div className="flex flex-col gap-4 p-4">
-          {books.map((book) => (
-            <Book key={book.isbn} book={book} />
-          ))}
+    <>
+      <Head>
+        <title>Search Book | Dog Eared</title>
+      </Head>
+      <div className="flex flex-col">
+        <div className="sticky top-0 z-10">
+          <input
+            className="w-full p-2"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
-      </InfiniteScrollContainer>
-    </div>
+
+        <InfiniteScrollContainer
+          onReachedEnd={fetchNextPage}
+          disabled={isFetching || !hasNextPage}
+        >
+          <div className="flex flex-col gap-4 p-4">
+            {books.map((book) => (
+              <Book key={book.isbn} book={book} />
+            ))}
+          </div>
+        </InfiniteScrollContainer>
+      </div>
+    </>
   );
 };
 
